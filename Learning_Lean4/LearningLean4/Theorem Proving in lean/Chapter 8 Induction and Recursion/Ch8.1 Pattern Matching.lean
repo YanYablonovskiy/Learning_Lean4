ae2d4ebc93ff6f73785495fea3d5c24bbbf320d1
YanYablonovskiy <yablonovskiy.yan@gmail.com>
2024-12-10 15:15:42 +1100

    Update Ch8.1 Pattern Matching.lean

--- Learning_Lean4/LearningLean4/Theorem Proving in lean/Chapter 8 Induction and Recursion/Ch8.1 Pattern Matching.lean
+++ Learning_Lean4/LearningLean4/Theorem Proving in lean/Chapter 8 Induction and Recursion/Ch8.1 Pattern Matching.lean
@@ -1,203 +1,212 @@
 /-
-As a further exercise, we suggest defining boolean operations and, or, not on the Bool type,
-and verifying common identities.
+Induction and Recursion:
 
-Note that you can define a binary operation like and using match:
--/
-def and (a: Bool) (b: Bool): Bool :=
- match a with
- | true => b
- | false => false
+In the previous chapter, we saw that inductive definitions provide a powerful means of introducing new types in Lean.
 
-def and_tactics (a: Bool) (b: Bool): Bool := by
-cases a
-. exact false
-. exact b
+Moreover, the constructors and the recursors provide the only means of defining functions on these types.
 
-def or (a: Bool) (b: Bool): Bool :=
-match a with
-| true => true
-| false => b
+By the propositions-as-types correspondence, this means that induction is the fundamental method of proof.
 
-def or_tactics (a: Bool)(b: Bool): Bool := by
-cases a
-. exact b
-. exact true
+Lean provides natural ways of defining recursive functions, performing pattern matching,
+and writing inductive proofs.
 
+It allows you to define a function by specifying equations that it should satisfy, and
+it allows you to prove a theorem by specifying how to handle various cases that can arise.
 
-def not (a: Bool): Bool :=
-match a with
-| true => false
-| false => true
+Behind the scenes, these descriptions are "compiled" down to primitive recursors, using
+a procedure that we refer to as the "equation compiler."
 
-def not_tactics (a: Bool): Bool := by
-cases a
-. exact true
-. exact false
-
-/-
-As exercises, we encourage you to develop a notion of composition for partial functions from α to β and β to γ,
-and show that it behaves as expected.
-
-We also encourage you to show that Bool and Nat are inhabited, that the product
-of two inhabited types is inhabited, and that the type of functions to an
-inhabited type is inhabited.
+The equation compiler is not part of the trusted code base; its output consists of terms that are
+checked independently by the kernel.
 -/
 
-universe u v w
-
-def compose_normal {α : Type u} {β : Type v} {γ: Type w} (f: α → β) (g: β → γ): α → γ :=
-fun (a:α) => g (f a)
-
-def compose_option {α : Type u} {β : Type v} {γ: Type w} (f: Option α → Option β) (g: Option β → Option γ): Option α → Option γ :=
-fun (a:Option α) => g (f a)
-
 /-
-As an exercise, prove the following:
+Pattern Matching:
+
+The interpretation of schematic patterns is the first step of the compilation process.
+
+We have seen that the casesOn recursor can be used to define functions and prove theorems by cases,
+according to the constructors involved in an inductively defined type.
+
+But complicated definitions may use several nested casesOn applications, and may be hard to read and understand.
+
+Pattern matching provides an approach that is more convenient, and familiar to
+users of functional programming languages.
+
+Consider the inductively defined type of natural numbers.
+
+Every natural number is either zero or succ x, and so you can define a function from the natural
+numbers to an arbitrary type by specifying a value in each of those cases:
 -/
-theorem append_nil (as : List α) : List.append as nil = as := by
- cases as
- . simp; admit
- . simp; admit
+open Nat
 
-theorem append_assoc (as bs cs : List α)
-        : List.append (List.append as bs) cs = List.append as (List.append bs cs) :=
-  by simp
+def sub1 : Nat → Nat --no := anymore
+  | zero   => zero
+  | succ x => x
 
-theorem symm {α : Type u} {a b : α} (h : Eq a b) : Eq b a :=
-  Eq.comm.mp h
-
-theorem symm_og {α : Type u} {a b : α} (h : Eq a b) : Eq b a :=
-  match h with
-  | rfl => rfl
-
-
-theorem trans {α : Type u} {a b c : α} (h₁ : Eq a b) (h₂ : Eq b c) : Eq a c := by
-cases h₁
-. assumption
-
-
-theorem congr_2 {α β : Type u} {a b : α} (f : α → β) (h : Eq a b) : Eq (f a) (f b) := by
-cases h
-. rfl
+def isZero : Nat → Bool
+  | zero   => true
+  | succ x => false
 
 /-
-Try defining other operations on the natural numbers, such as multiplication, the predecessor function (with pred 0 = 0),
-truncated subtraction (with n - m = 0 when m is greater than or equal to n), and exponentiation.
+The equations used to define these functions hold definitionally:
+-/
+example : sub1 0 = 0 := rfl
+example (x : Nat) : sub1 (succ x) = x := rfl
 
-Then try proving some of their basic properties, building on the theorems we have already proved.
+example : isZero 0 = true := rfl
+example (x : Nat) : isZero (succ x) = false := rfl
 
-Since many of these are already defined in Lean's core library, you should work within a namespace named Hidden,
-or something like that, in order to avoid name clashes.
+example : sub1 7 = 6 := rfl
+example (x : Nat) : isZero (x + 3) = false := rfl
+
+/-
+Instead of zero and succ, we can use more familiar notation:
+-/
+def subone : Nat → Nat
+  | 0   => 0
+  | x+1 => x
+
+def isZero' : Nat → Bool
+  | 0   => true
+  | x+1 => false
+
+/-
+Because addition and the zero notation have been assigned the [match_pattern] attribute,
+they can be used in pattern matching.
+
+Lean simply normalizes these expressions until the constructors zero and succ are exposed.
+
+Pattern matching works with any inductive type, such as products and option types:
+-/
+def swap : α × β → β × α
+  | (a, b) => (b, a)
+
+def foo : Nat × Nat → Nat
+  | (m, n) => m + n
+
+def bar : Option Nat → Nat
+  | some n => n + 1
+  | none   => 0
+
+/-
+Here we use it not only to define a function, but also to carry out a proof by cases:
 -/
 
 namespace Hidden
 
+def not : Bool → Bool
+  | true  => false
+  | false => true
 
---multiplication
-def mul (a:Nat)(b:Nat): Nat :=
- match b with
- | Nat.zero => Nat.zero
- | Nat.succ k => (mul a k) + a --for some reason this recursion works
-
-
-def mul_tac (a:Nat)(b:Nat): Nat := by
-cases b
-. exact Nat.zero
-. case succ k => exact (mul a k) + a
-
-
-#eval mul 3 4
-
---the predecessor function (with pred 0 = 0)
-
-def pred (a:Nat): Nat :=
-match a with
-| Nat.zero => Nat.zero
-| Nat.succ k => k
-
-def pred_tactic (a: Nat): Nat := by
-cases a
-. exact Nat.zero
-. assumption
-
-
-#eval pred 6
-
-#eval pred_tactic 6
-
---truncated subtraction (with n - m = 0 when m is greater than or equal to n)
-
--- def trunc_sub (n: Nat)(m: Nat): Nat := by
--- cases m
--- . exact n
--- . case succ k => exact trunc_sub n (pred k)
-
-
-
-def exp (n: Nat)(m: Nat): Nat :=
-match m with
-| Nat.zero => Nat.succ (Nat.zero)
-| Nat.succ k => mul (exp n k) n--n^(k+1) ; recursion worked this time
-
-def exp_tactic (n: Nat)(m: Nat): Nat := by
-cases m
-. exact Nat.succ (Nat.zero)
-. case succ k => exact mul (exp n k) n
+theorem not_not : ∀ (b : Bool), not (not b) = b
+  --like match on the existential
+  | true  => rfl  -- proof that not (not true) = true
+  | false => rfl  -- proof that not (not false) = false
 
 /-
-Define some operations on lists, like a length function or the reverse function.
-
-Prove some properties, such as the following:
-
-a. length (s ++ t) = length s + length t
-
-b. length (reverse t) = length t
-
-c. reverse (reverse t) = t
+Pattern matching can also be used to destruct inductively defined propositions:
 -/
+example (p q : Prop) : p ∧ q → q ∧ p
+  | And.intro h₁ h₂ => And.intro h₂ h₁ --because p ∧ q can only be of one constructor
 
-variable {α: Type u}
+example (p q : Prop) : p ∨ q → q ∨ p
+  | Or.inl hp => Or.inr hp --taking care of both constructors
+  | Or.inr hq => Or.inl hq
 
-def length (as: List α): Nat :=
- match as with
- | List.nil => Nat.zero
- | List.cons _ ks => Nat.succ (length ks)
-
-def length_tactic (as: List α): Nat := by
-cases as
-. exact Nat.zero
-. case cons head tail => exact Nat.succ (length_tactic tail)
-
-
-
-def reverse (as: List α): List α :=
-match as with
-| List.nil => List.nil
-| List.cons a bs => sorry
-
-def reverse_tactic (as: List α): List α := by
-cases as
-. exact List.nil
-. case cons head tail => admit
-
-
-
--- example (s: List α) (t: List α): length (s ++ t) = length s + length t :=
--- match t with
--- | List.nil => sorry
---  sorry
 
 /-
-Define an inductive data type consisting of terms built up from the following constructors:
+This provides a compact way of unpacking hypotheses that make use of logical connectives.
 
-const n, a constant denoting the natural number n
-var n, a variable, numbered n
-plus s t, denoting the sum of s and t
-times s t, denoting the product of s and t
+In all these examples, pattern matching was used to carry out a single case distinction.
 
-Recursively define a function that evaluates any such term with respect to
-an assignment of values to the variables.
+More interestingly, patterns can involve nested constructors, as in the following examples.
 -/
-inductive Terms where
-|const: Nat → Terms
+
+def sub2 : Nat → Nat
+  | 0   => 0
+  | 1   => 0
+  | x+2 => x
+
+/-
+The equation compiler first splits on cases as to whether the input is zero or of the form succ x.
+
+--if succ x, checks whether its succ zero (1)
+
+It then does a case split on whether x is of the form zero or succ x'.
+
+It determines the necessary case splits from the patterns that are presented to it, and raises an error
+if the patterns fail to exhaust the cases.
+
+Once again, we can use arithmetic notation, as in the version below.
+
+In either case, the defining equations hold definitionally.
+-/
+
+example : sub2 0 = 0 := rfl
+example : sub2 1 = 0 := rfl
+example : sub2 (x+2) = x := rfl
+
+example : sub2 5 = 3 := rfl
+
+/-
+You can write #print sub2 to see how the function was compiled to recursors.
+
+(Lean will tell you that sub2 has been defined in terms of an internal auxiliary function,
+sub2.match_1, but you can print that out too.)
+
+Lean uses these auxiliary functions to compile match expressions.
+
+Actually, the definition above is expanded to
+-/
+
+#print sub2
+
+def sub2Full : Nat → Nat :=
+  fun x =>
+    match x with
+    | 0   => 0
+    | 1   => 0
+    | x+2 => x
+
+/-
+Here are some more examples of nested pattern matching:
+-/
+example (p q : α → Prop)
+        : (∃ x, p x ∨ q x) → (∃ x, p x) ∨ (∃ x, q x)
+  | Exists.intro x (Or.inl px) => Or.inl (Exists.intro x px)
+  | Exists.intro x (Or.inr qx) => Or.inr (Exists.intro x qx)
+
+def foo : Nat × Nat → Nat
+  | (0, n)     => 0
+  | (m+1, 0)   => 1
+  | (m+1, n+1) => 2
+
+/-
+The equation compiler can process multiple arguments sequentially.
+
+For example, it would be more natural to define the previous example as a function of two arguments:
+-/
+
+def foo1 : Nat → Nat → Nat
+  | 0,   n   => 0
+  | m+1, 0   => 1
+  | m+1, n+1 => 2
+
+/-
+Here is another example:
+-/
+def bar : List Nat → List Nat → Nat
+  | [],      []      => 0
+  | a :: as, []      => a  --constructor with a as argument; a :: as
+  | [],      b :: bs => b
+  | a :: as, b :: bs => a + b
+
+--function that returns the sum of the heads of two lists
+-- defined as 0 if the list is empty
+
+/-
+Note that the patterns are separated by commas.
+
+In each of the following examples, splitting occurs on only the first argument, even though the others are included among the list of patterns.
+-/
