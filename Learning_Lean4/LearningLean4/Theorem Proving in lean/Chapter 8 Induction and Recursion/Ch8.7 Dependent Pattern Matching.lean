91027bdd883c765872a1e967235cc6bcdb54dfec
YanYablonovskiy <yablonovskiy.yan@gmail.com>
2024-12-26 19:36:17 +1100

    Update Ch8.7 Dependent Pattern Matching.lean

--- Learning_Lean4/LearningLean4/Theorem Proving in lean/Chapter 8 Induction and Recursion/Ch8.7 Dependent Pattern Matching.lean
+++ Learning_Lean4/LearningLean4/Theorem Proving in lean/Chapter 8 Induction and Recursion/Ch8.7 Dependent Pattern Matching.lean
@@ -40,4 +40,30 @@ end Vector
 
 
 
+
+
+/-
+But what value should we return in the nil case?
+
+Something funny is going on: if v has type Vector α (succ n), it can't be nil,
+but it is not clear how to tell that to casesOn.
+
+One solution is to define an auxiliary function:
+-/
+
+def tailAux (v : Vector α m) : m = n + 1 → Vector α n :=
+  Vector.casesOn (motive := fun x _ => x = n + 1 → Vector α n) v --v is the vector; motive is a vector not of length zero to its tail
+    (fun h : 0 = n + 1 => Nat.noConfusion h)
+    (fun (a : α) (m : Nat) (as : Vector α m) =>
+     fun (h : m + 1 = n + 1) =>
+       Nat.noConfusion h (fun h1 : m = n => h1 ▸ as))
+
+def tail (v : Vector α (n+1)) : Vector α n :=
+  tailAux v rfl
+
+
+
+
+
+
 end Hidden
